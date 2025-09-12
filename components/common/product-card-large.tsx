a6ba4e5a167fbe1fe37c173fdedac7d387624953
komikodok <komikodok666@gmail.com>
2025-09-12 20:17:21 +0700

    feat: fix landing-page

--- components/common/product-card-large.tsx
+++ components/common/product-card-large.tsx
@@ -18,42 +18,42 @@ const ProductCardLarge = ({ product }: ProductCardProps) => {
   return (
     <li
       ref={cardRef}
-      className="relative flex flex-col max-md:scale-75 w-52 h-76 md:w-72 md:h-92 row-span-3 border border-stone-300 rounded-md p-1"
+      className="relative flex flex-col w-40 h-60 md:w-72 md:h-92 row-span-3 border border-stone-300 rounded-md p-1"
     >
-      <div className="relative flex-shrink-0 w-full h-42 md:h-48 rounded-md">
+      <div className="relative flex-shrink-0 w-full h-30 md:h-48 rounded-md">
         <Image
           alt="product"
           src={product.image}
           fill
           className="object-contain"
-          sizes="(max-width: 768px) 208px, (max-width: 1200px) 288px, 288px"
+          sizes="(max-width: 768px) 160px, (max-width: 1200px) 120px, 120px"
         />
         <WishlistButton />
       </div>
 
       <div className="flex flex-col px-2 py-1 md:p-3">
-        <h3 className="font-medium text-xs text-stone-600">{product.category}</h3>
-        <h3 className="font-semibold text-sm line-clamp-2">{product.title}</h3>
+        <h3 className="font-medium text-[8px] md:text-xs text-stone-600">{product.category}</h3>
+        <h3 className="font-semibold text-[9px] md:text-sm line-clamp-2">{product.title}</h3>
       </div>
 
       <div className="flex items-center p-1 md:px-3 md:py-2">
-        <Star className="size-5" fill="#FFA500" strokeWidth={0} />
-        <p className="text-xs ml-1 font-medium text-black/70">{product.rating.rate}</p>
-        <div className="h-4 mx-2">
+        <Star className="size-3 md:size-5" fill="#FFA500" strokeWidth={0} />
+        <p className="text-[8px] md:text-xs ml-1 font-medium text-black/70">{product.rating.rate}</p>
+        <div className="h-3 md:h-4 mx-2">
           <Separator orientation="vertical" className="bg-black/70 w-4" />
         </div>
-        <p className="text-xs font-medium text-black/70">{product.rating.count} review</p>
+        <p className="text-[8px] md:text-xs font-medium text-black/70">{product.rating.count} review</p>
       </div>
 
       <div
-        className="relative group cursor-pointer flex justify-center items-center mx-auto w-40 md:w-56 h-6 md:h-8 border border-stone-400 rounded-full"
-        onClick={() => console.log("click")}
+        className="relative group cursor-pointer flex justify-center items-center mx-auto w-30 md:w-56 h-6 md:h-8 border border-stone-400 rounded-full"
+        onClick={() => {}}
       >
         <div className="overlay absolute z-0 left-0 w-0 h-full group-hover:w-full group-active:w-full bg-blue-900 rounded-full transition-all md:duration-300"></div>
         <div className="absolute right-0 w-8 h-8 flex justify-center items-center rounded-full ring-3 ring-white bg-blue-900">
           <Plus className="w-5 h-5 text-white" />
         </div>
-        <h2 className="text-blue-950 group-hover:text-white group-active:text-white text-sm font-bold z-20 transition-all md:duration-300">
+        <h2 className="text-blue-950 group-hover:text-white group-active:text-white text-[9px] md:text-sm font-bold z-20 transition-all md:duration-300">
           ${product.price}
         </h2>
       </div>
--- components/common/product-card-regular.tsx
+++ components/common/product-card-regular.tsx
@@ -18,42 +18,42 @@ const ProductCardRegular = ({ product }: ProductCardProps) => {
     return (
         <li 
             ref={cardRef}
-            className="max-md:scale-75 w-52 h-28 md:w-72 md:h-32 border border-stone-300 rounded-md p-1"
+            className="w-40 h-24 md:w-72 md:h-32 border border-stone-300 rounded-md p-1"
         >
             <div className="relative flex">
-                <div className="relative flex-shrink-0 w-15 h-15 md:w-16 md:h-16">
+                <div className="relative flex-shrink-0 w-10 h-10 md:w-16 md:h-16">
                     <Image 
                         alt="product" 
                         src={product.image} 
                         className="object-contain"
                         fill
-                        sizes="(max-width: 768px) 60px, 64px"
+                        sizes="(max-width: 768px) 40px, 64px"
                     />
             </div>
 
             <div className="w-full px-3 py-1">
-                <h3 className="text-[9px] text-stone-600">{product.category}</h3>
-                <h3 className="text-[10px] font-semibold line-clamp-1">{product.title}</h3>
+                <h3 className="text-[7px] md:text-[9px] text-stone-600">{product.category}</h3>
+                <h3 className="text-[8px] md:text-[10px] font-semibold line-clamp-1">{product.title}</h3>
 
                 <div className="flex items-center mt-2">
-                    <Star className="size-3" fill="#FFA500" strokeWidth={0} />
-                    <p className="text-[9px] ml-1 font-medium text-black/70">{product.rating.rate}</p>
-                    <div className="h-3 mx-2">
+                    <Star className="size-2 md:size-3" fill="#FFA500" strokeWidth={0} />
+                    <p className="text-[7px] md:text-[9px] ml-1 font-medium text-black/70">{product.rating.rate}</p>
+                    <div className="h-2 md:h-3 mx-2">
                         <Separator orientation="vertical" className="bg-black/70 w-4"/>
                     </div>
-                        <p className="text-[9px] font-medium text-black/70">{product.rating.count} review</p>
+                        <p className="text-[7px] md:text-[9px] font-medium text-black/70">{product.rating.count} review</p>
                     </div>
                 </div>
             
                 <WishlistButton></WishlistButton>
             </div>
 
-            <div className="relative group cursor-pointer flex justify-center items-center mx-auto my-2 w-40 md:w-48 h-6 md:h-8 border border-stone-400 rounded-full">
+            <div className="relative group cursor-pointer flex justify-center items-center mx-auto my-2 w-30 md:w-48 h-5 md:h-8 border border-stone-400 rounded-full">
                 <div className="overlay absolute z-0 left-0 w-0 h-full group-hover:w-full group-active:w-full bg-blue-900 rounded-full transition-all duration-300"></div>
-                <div className="absolute right-0 w-8 h-8 flex justify-center items-center rounded-full ring-3 ring-white bg-blue-900">
+                <div className="absolute right-0 w-6 md:w-8 h-6 md:h-8 flex justify-center items-center rounded-full ring-3 ring-white bg-blue-900">
                     <Plus className="w-5 h-5 text-white"/>
                 </div>
-                <h2 className="text-blue-950 group-hover:text-white group-active:text-white text-sm font-bold z-20 transition-all duration-300">${product.price}</h2>
+                <h2 className="text-blue-950 group-hover:text-white group-active:text-white text-[9px] md:text-sm font-bold z-20 transition-all duration-300">${product.price}</h2>
             </div>
         </li>
     )
