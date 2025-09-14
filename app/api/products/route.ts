f493abce95c5cb14553e2da16c0a9dd723d385d7
komikodok <komikodok666@gmail.com>
2025-09-14 23:19:44 +0700

    feat: real fix landing-page

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,38 +1,30 @@
 import { BASE_FAKESTORE_API_URL } from "@/lib/base-url";
 import { IProducts } from "@/lib/types/products.type";
-// import axios from "axios";
-import { NextResponse, NextRequest } from "next/server";
+import axios from "axios";
+import { NextResponse } from "next/server";
 import { logger } from "@/lib/logger";
 
 
-export async function GET(req: NextRequest) {
+export async function GET(req: Request) {
     const { searchParams } = new URL(req.url)
     try {
         const limit = searchParams.get("limit") || undefined
         const sort = searchParams.get("sort") || undefined
 
-        // const res = await axios.get<IProducts[]>(`${BASE_FAKESTORE_API_URL}/products`, {
-        //     params: {
-        //         limit: limit,
-        //         sort: sort
-        //     }
-        // })
-        const res = await fetch(`${BASE_FAKESTORE_API_URL}/products?limit=${limit}&sort=${sort}`, {
-            method: "GET",
-            next: {
-                revalidate: 60
+        const res = await axios.get<IProducts[]>(`${BASE_FAKESTORE_API_URL}/products`, {
+            params: {
+                limit: limit,
+                sort: sort
             }
         })
 
-        const resData: IProducts[] = await res.json()
-
         if (res.status !== 200) {
-            logger.error("Failed fetch data", resData)
+            logger.error("Failed fetch data", res.data)
 
             return NextResponse.json({ error: "Failed fetch data" }, { status: res.status })
         }
         
-        return NextResponse.json(resData)
+        return NextResponse.json(res.data)
     } catch (err) {
         logger.error("Internal server error", err)
         return NextResponse.json({ error: err }, { status: 500 })
