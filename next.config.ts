204ed5442bc9a572c5c003a9e11e5bda0f2491d3
komikodok <komikodok666@gmail.com>
2025-11-27 15:40:04 +0700

    Revert "feat: /main fix"
    This reverts commit 9da6ec0025e25feca8d7a78c3f4111a4654805ef.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ["fakestoreapi.com"]
-  },
-  reactStrictMode: false
+  }
 };
 
 export default nextConfig;
